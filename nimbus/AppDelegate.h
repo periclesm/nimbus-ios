3ddc1632d1ce6696d902749cfd4e564f8f085ecb
Pericles Maravelakis <periclesm@cloudfields.net>
2017-04-19 10:38:30 +0300

    Initial Commit

---
+++ nimbus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nimbus
+//
+//  Created by Pericles Maravelakis on 19/04/2017.
+//  Copyright © 2017 Cloudfields. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nimbus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nimbus
+//
+//  Created by Pericles Maravelakis on 19/04/2017.
+//  Copyright © 2017 Cloudfields. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
