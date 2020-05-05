4110da734ae3d9e33fb7c6426cf0e576bf22914e
Pericles Maravelakis <periclesm@cloudfields.net>
2020-05-05 12:37:13 +0300

    Minor Reformats in ObjC code

--- nimbus/Controller/DataLogic.m
+++ nimbus/Controller/DataLogic.m
@@ -12,29 +12,27 @@
 
 @implementation DataLogic
 
-+ (void)GetInitialData
-{
-	[Networker GetDataFromService:cloudList parameters:nil completion:^(id response) {
-//		NSLog(@"response: %@", response);
-		NSArray *datamap = [DataMapper DataMapForEntity:DataEntityList object:(NSDictionary *)response];
-		[DataQuery SetItemsToEntity:@"CloudList" items:datamap];
-	}];
-	
-	[Networker GetDataFromService:cloudType parameters:nil completion:^(id response) {
-//		NSLog(@"response: %@", response);
-		NSArray *datamap = [DataMapper DataMapForEntity:DataEntityType object:(NSDictionary *)response];
-		[DataQuery SetItemsToEntity:@"CloudType" items:datamap];
-	}];
-	
-	[Networker GetDataFromService:cloudDetail parameters:nil completion:^(id response) {
-//		NSLog(@"response: %@", response);
-		NSArray *datamap = [DataMapper DataMapForEntity:DataEntityDetail object:(NSDictionary *)response];
-		[DataQuery SetItemsToEntity:@"CloudDetail" items:datamap];
-	}];
++ (void)GetInitialData {
+    [Networker GetDataFromService:cloudList parameters:nil completion:^(id response) {
+//        NSLog(@"response: %@", response);
+        NSArray *datamap = [DataMapper DataMapForEntity:DataEntityList object:(NSDictionary *)response];
+        [DataQuery SetItemsToEntity:@"CloudList" items:datamap];
+    }];
+
+    [Networker GetDataFromService:cloudType parameters:nil completion:^(id response) {
+//        NSLog(@"response: %@", response);
+        NSArray *datamap = [DataMapper DataMapForEntity:DataEntityType object:(NSDictionary *)response];
+        [DataQuery SetItemsToEntity:@"CloudType" items:datamap];
+    }];
+
+    [Networker GetDataFromService:cloudDetail parameters:nil completion:^(id response) {
+//        NSLog(@"response: %@", response);
+        NSArray *datamap = [DataMapper DataMapForEntity:DataEntityDetail object:(NSDictionary *)response];
+        [DataQuery SetItemsToEntity:@"CloudDetail" items:datamap];
+    }];
 }
 
-+ (NSString*)GetCloudType:(NSString*)objectId
-{
++ (NSString*)GetCloudType:(NSString*)objectId {
 	NSString *predicateString = [NSString stringWithFormat:@"objectId == '%@'", objectId];
 	NSPredicate *predicate = [NSPredicate predicateWithFormat:predicateString];
 	NSArray *cloudtype = [DataQuery SearchItemsFromEntity:@"CloudType" predicate:predicate];
@@ -44,8 +42,7 @@
 	return ct.name;
 }
 
-+ (CloudList*)GetCloudInfo:(NSString*)objectId
-{
++ (CloudList*)GetCloudInfo:(NSString*)objectId {
 	NSString *predicateString = [NSString stringWithFormat:@"objectId == '%@'", objectId];
 	NSPredicate *predicate = [NSPredicate predicateWithFormat:predicateString];
 	NSArray *cloudListData = [DataQuery SearchItemsFromEntity:@"CloudList" predicate:predicate];
@@ -53,16 +50,14 @@
 	return cloudListData.firstObject;
 }
 
-+ (NSString*)GetCloudDetails:(NSString*)objectId shortText:(Boolean)shortText
-{
++ (NSString*)GetCloudDetails:(NSString*)objectId shortText:(Boolean)shortText {
 	NSString *predicateString = [NSString stringWithFormat:@"objectId == '%@'", objectId];
 	NSPredicate *predicate = [NSPredicate predicateWithFormat:predicateString];
 	NSArray *clouddetail = [DataQuery SearchItemsFromEntity:@"CloudDetail" predicate:predicate];
 	
 	CloudDetail *cd = clouddetail.firstObject;
 	
-	if (shortText)
-	{
+	if (shortText) {
 		if ([cd.detail rangeOfString:@"."].location != NSNotFound)
 			return [cd.detail substringToIndex:[cd.detail rangeOfString:@"."].location + 1];
 		else
@@ -72,8 +67,7 @@
 		return cd.detail;
 }
 
-+ (NSString*)GetCloudImageURL:(NSString*)objectId
-{
++ (NSString*)GetCloudImageURL:(NSString*)objectId {
 	NSString *predicateString = [NSString stringWithFormat:@"objectId == '%@'", objectId];
 	NSPredicate *predicate = [NSPredicate predicateWithFormat:predicateString];
 	NSArray *clouddetail = [DataQuery SearchItemsFromEntity:@"CloudDetail" predicate:predicate];
@@ -83,8 +77,7 @@
 	return cd.image;
 }
 
-+ (NSString*)GetCloudWikiURL:(NSString*)objectId
-{
++ (NSString*)GetCloudWikiURL:(NSString*)objectId {
 	NSString *predicateString = [NSString stringWithFormat:@"objectId == '%@'", objectId];
 	NSPredicate *predicate = [NSPredicate predicateWithFormat:predicateString];
 	NSArray *clouddetail = [DataQuery SearchItemsFromEntity:@"CloudDetail" predicate:predicate];
