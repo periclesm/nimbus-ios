21353cae12eac4a9f9e34bd2bfc5a88784b7edc7
Perikles Maravelakis <periclesm@cloudfields.net>
2021-12-14 12:06:06 +0200

    Replaced AppDelegate.m with a swift version

---
+++ nimbus/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  nimbus
+//
+//  Created by Perikles Maravelakis on 14/12/21.
+//  Copyright © 2021 Cloudfields. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	var window: UIWindow?
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+
+}
