3adcf35d91d8185b5d911eb48676d5fa6a0aedfc
Perikles Maravelakis <periclesm@cloudfields.net>
2022-02-28 00:28:57 +0200

    AppDelegate is now in Swift

---
+++ nimbus/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  nimbus
+//
+//  Created by Perikles Maravelakis on 26/2/22.
+//  Copyright © 2022 Cloudfields. All rights reserved.
+//
+//	Licensed under Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+//	https://creativecommons.org/licenses/by-sa/4.0/
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	var window: UIWindow?
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	
+		return true
+	}
+}
